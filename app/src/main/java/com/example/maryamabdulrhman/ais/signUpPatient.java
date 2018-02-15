8c9a96035a69b2f9e5ae232008403f2ca87342e1
Maryam Abdulrhman <maryamabdulrhman@Maryams-MacBook-Pro.local>
2018-02-15 15:53:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/maryamabdulrhman/ais/signUpPatient.java
@@ -0,0 +1,13 @@
+package com.example.maryamabdulrhman.ais;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signUpPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_patient);
+    }
+}
