9b68322ec22d26ac08b91dd231d799bb9989b604
maryamabdulrhman <sd72@windowslive.com>
2018-02-20 19:19:11 +0300

    add navigation

---
+++ app/src/main/java/com/example/maryamabdulrhman/ais/MainActivityPatient.java
@@ -0,0 +1,60 @@
+package com.example.maryamabdulrhman.ais;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivityPatient extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_patient);
+        loadFragment(new PatientProfile());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.MyProfile:
+                fragment = new PatientProfile();
+                break;
+
+            case R.id.outbreak:
+                fragment = new fragmentOutbreaks();
+                break;
+
+            case R.id.reportFlu:
+                fragment = new FragmentReportFlu();
+                break;
+
+            case R.id.mydoctor:
+                fragment = new fragmenMydoctor();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/maryamabdulrhman/ais/fragmenMydoctor.java
@@ -0,0 +1,18 @@
+package com.example.maryamabdulrhman.ais;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class fragmenMydoctor extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_fragmen_mydoctor, null);
+    }
+}
