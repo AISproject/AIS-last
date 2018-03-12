2510904f0ceed22228196e7ce927d9734308b824
maryamabdulrhman <sd72@windowslive.com>
2018-03-12 19:46:55 +0300

    edit

---
+++ app/src/main/java/com/example/maryamabdulrhman/ais/ReportCoronaActivity.java
@@ -0,0 +1,13 @@
+package com.example.maryamabdulrhman.ais;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportCoronaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_corona);
+    }
+}
