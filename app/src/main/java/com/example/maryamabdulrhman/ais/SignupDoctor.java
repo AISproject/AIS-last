801558da50ebb48ce185b01f61a2fa1e1b4614f1
maryamabdulrhman <sd72@windowslive.com>
2018-03-13 17:25:24 +0300

    update date to calander

--- app/src/main/java/com/example/maryamabdulrhman/ais/SignupDoctor.java
+++ app/src/main/java/com/example/maryamabdulrhman/ais/SignupDoctor.java
@@ -1,13 +1,52 @@
 package com.example.maryamabdulrhman.ais;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class SignupDoctor extends AppCompatActivity {
+    private static final String TAG="SignupDoctor";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_doctor);
+        mDisplayDate= (TextView) findViewById(R.id.bdate);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                int year=c.get(Calendar.YEAR);
+                int month=c.get(Calendar.MONTH);
+                int day=c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        SignupDoctor.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month=month+1;
+                Log.d(TAG,"onDateSet: mm/dd/yyy" + month + "/" + day + "/" + year );
+                String date= month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
     }
 }
