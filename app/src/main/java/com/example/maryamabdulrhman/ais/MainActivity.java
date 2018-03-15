a2660ba4e328cfa28e0a86a0805e89d01453b1cf
nourayg <noura.y.algaraawi@gmail.com>
2018-03-15 23:32:33 +0300

    added

--- app/src/main/java/com/example/maryamabdulrhman/ais/MainActivity.java
+++ app/src/main/java/com/example/maryamabdulrhman/ais/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.maryamabdulrhman.ais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
 }
