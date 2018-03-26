47a67f068d911d06778109423537685cdb061f05
nourayg <noura.y.algaraawi@gmail.com>
2018-03-26 03:14:31 +0300

    hathi menu 7gt age mdri etha ok wla la bs 3yt tsht'3l 3ndi

--- app/src/main/java/com/example/maryamabdulrhman/ais/MainActivity.java
+++ app/src/main/java/com/example/maryamabdulrhman/ais/MainActivity.java
@@ -4,19 +4,47 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// for the age range
+        Spinner spinner = findViewById(R.id.Agespinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Age, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        //there's somthing missing
+
+
+
+
+
 
 
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+       String eSelected=adapterView.getItemAtPosition(position).toString();
+        Toast.makeText(this, eSelected,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
